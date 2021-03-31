d10fb2fcb13eb6583fb9b20d40b0bc14d05e7c92
Lucas Mobley <Lucas@LucasMobley.com>
2021-03-30 19:39:51 -0700

    rendering!

--- client/App.jsx
+++ client/App.jsx
@@ -1,29 +1,36 @@
-import React, { useMemo, useRef } from 'react'
-import styles from './App.module.scss';
-import { Canvas, useFrame } from 'react-three-fiber'
+
+
+
+
+
+
+
+// import React, { useMemo, useRef } from 'react'
+// import styles from './App.module.scss';
+// import { Canvas, useFrame } from 'react-three-fiber'
 // import { Effects } from './Effects'
 // import { ResizeObserver } from '@juggle/resize-observer'
-import * as THREE from 'three'
+// import * as THREE from 'three'
 
-// --- original code
+// // --- original code
+// // const App = () => {
+// //   return (
+// //     <div>
+// //       <h1>hiiiiii World</h1>
+// //     </div>
+// //   )
+// // }
+
+// // hanging a Canvas element with a black background 
+// // #### not the entire page... MAKE ADJUSTMENTS
+// // <<< User option => background color >>>
+
 // const App = () => {
 //   return (
-//     <div>
-//       <h1>hiiiiii World</h1>
-//     </div>
+//     <Canvas>
+//       <color attach="background" args = {['lightcyan']} />  
+//     </Canvas>
 //   )
 // }
 
-// hanging a Canvas element with a black background 
-// #### not the entire page... MAKE ADJUSTMENTS
-// <<< User option => background color >>>
-
-const App = () => {
-  return (
-    <Canvas>
-      <color attach="background" args = {['lightcyan']} />  
-    </Canvas>
-  )
-}
-
-export default App;
+// export default App;
